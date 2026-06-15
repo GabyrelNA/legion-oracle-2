2ad65fcc5f9a2cc2bfe893b94a483dec5ccc9e8c
GabyrelNA <navarroazpiroz@gmail.com>
2026-06-15 14:12:54 +0200

    Update buscador.js

--- src/buscador.js
+++ src/buscador.js
@@ -1,11 +1,36 @@
 export function buscarReglas(reglas, pregunta) {
   const texto = pregunta.toLowerCase();
 
-  const coincidencias = reglas.filter((regla) =>
-    regla.palabras.some((palabra) =>
-      texto.includes(palabra.toLowerCase())
-    )
-  );
+  const coincidencias = reglas.filter((regla) => {
+    const coincidePalabras =
+      regla.palabras?.some((palabra) =>
+        texto.includes(
+          palabra.toLowerCase()
+        )
+      );
+
+    const coincideTitulo =
+      regla.titulo
+        ?.toLowerCase()
+        .includes(texto);
+
+    const coincideRespuesta =
+      regla.respuesta
+        ?.toLowerCase()
+        .includes(texto);
+
+    const coincideDocumento =
+      regla.documento
+        ?.toLowerCase()
+        .includes(texto);
+
+    return (
+      coincidePalabras ||
+      coincideTitulo ||
+      coincideRespuesta ||
+      coincideDocumento
+    );
+  });
 
   coincidencias.sort(
     (a, b) => b.autoridad - a.autoridad
