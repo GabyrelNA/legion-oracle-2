b444f647365e0c995d5b7903850a4805a832cdb6
GabyrelNA <navarroazpiroz@gmail.com>
2026-06-17 08:41:31 +0200

    Update importarReglas.js

--- scripts/importarReglas.js
+++ scripts/importarReglas.js
@@ -1,1 +1,12 @@
-// Importador de reglas
+const fs = require("fs");
+
+console.log("Importador preparado");
+
+const reglas = [];
+
+fs.writeFileSync(
+  "./public/rulebook2025.json",
+  JSON.stringify(reglas, null, 2)
+);
+
+console.log("JSON generado");
